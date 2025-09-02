eca4716e0e8a2ed7dd47aad097d6aff380e6f6b9
MUKESHKUMAR M <mukeshkumar.cse24@gmail.com>
2025-09-02 21:41:37 +0530

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Any request from your React app starting with /api...
+      '/api': {
+        // ...will be forwarded to your live Render backend
+        target: 'https://sm-poc-backend.onrender.com',
+        changeOrigin: true, // This is important for virtual hosts
+      },
+    },
+  },
 })
